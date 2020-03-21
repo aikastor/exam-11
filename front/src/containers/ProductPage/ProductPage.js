dddd16ecc79dfb2a2fb6a8247a5ce803c0c07110
aikast <aikasat95@gmail.com>
2020-03-21 17:02:53 +0600

    - changed layout for single product component

--- front/src/containers/ProductPage/ProductPage.js
+++ front/src/containers/ProductPage/ProductPage.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {deleteProduct, fetchProduct} from "../../store/actions/productsActions";
 import {connect} from "react-redux";
 import {apiURL} from "../../constants";
-import {Alert, Button} from "reactstrap";
+import {Alert, Button, Col, Row} from "reactstrap";
 
 class ProductPage extends Component {
   componentDidMount() {
@@ -18,29 +18,36 @@ class ProductPage extends Component {
         {this.props.error && (
           <Alert color="danger">{this.props.error.error}</Alert>
         )}
+
         <h1>{product.title}</h1>
         <h4>Price: {product.price}</h4>
-        {
-          this.props.user &&
-            this.props.user._id === product.seller._id &&
-          <Button
-            color="danger"
-            className="float-right"
-            onClick={()=>{this.props.deleteProduct(product._id)}}
-          >
-            Delete
-          </Button>
-        }
-        <img src={apiURL + '/uploads/' + product.image} alt={product.title}
-        height='400px'/>
-        <p>{product.description}</p>
-        Seller:
-        {
-          product.seller.displayName
-        }
-        {
-          product.seller.phone
-        }
+        <Row>
+          <Col xs={8}>
+            <img src={apiURL + '/uploads/' + product.image} alt={product.title}
+                 height='400px'/>
+            <p>{product.description}</p>
+          </Col>
+          <Col xs={4}>
+            {
+              this.props.user &&
+              this.props.user._id === product.seller._id &&
+              <Button
+                color="danger"
+                className="float-right"
+                onClick={()=>{this.props.deleteProduct(product._id)}}
+              >
+                Delete
+              </Button>
+            }
+            Seller:
+            <p><b>{product.seller.displayName}</b></p>
+            Phone:
+            <p><b>{product.seller.phone}</b></p>
+          </Col>
+        </Row>
+
+
+
       </div>
     );
   }
